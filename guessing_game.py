65f40a380df259f275fae7751488955f4b24f23e
Imanol Vazquez <dom.vaz333@gmail.com>
2019-06-11 22:49:59 -0700

    attempts counter was added

--- guessing_game.py
+++ guessing_game.py
@@ -3,25 +3,27 @@ import os
 os.system('cls')
 
 
-def help(num_to_guess, num_try):
+def help(num_to_guess, num_try, score):
 	if num_try != 0:
 		if num_try < num_to_guess:
-			print("you are lower")
+			print("you are lower. number of attempts {}".format(score))
 		elif num_try > num_to_guess:
-			print("you are higher")
+			print("you are higher. number of attempts {}".format(score))
 	else:
 		print("WELLCOME TO THE GAME")
 
 def start_game():
 	num_to_guess = random.randint(1, 10)
 	num_try = 0
+	score = 0
 	print("HI")
 	while num_to_guess != num_try:
-		help(num_to_guess, num_try)
+		help(num_to_guess, num_try, score)
+		score = score + 1
 		num_try = input("tell me a number..")
 		num_try = int(num_try)
 		os.system('cls')
-	print("gg wp")
+	print("gg wp. yout final score is {}".format(score))
 	r = input("Would you like to play again? (y/n)")
 	if r == "yes":
 		num_try = 0
